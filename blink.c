03bf388b3705a584e942110d08cdcbfd183e3d56
Joe Nelson <joe@begriffs.com>
2022-10-22 20:34:25 -0500

    FreeRTOS POSIX requires dynamic memory...

--- blink.c
+++ blink.c
@@ -1,24 +1,27 @@
-#include <FreeRTOS.h>
-#include <task.h>
-
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 
-StaticTask_t taskBuf;
-StackType_t  taskStack[configMINIMAL_STACK_SIZE];
+#include <FreeRTOS.h>
+#include <task.h>
 
-void blink(void *args)
+#include <FreeRTOS_POSIX/pthread.h>
+#include <FreeRTOS_POSIX/unistd.h>
+
+void *blink(void *args)
 {
 	(void)args;
 	while (1)
 	{
 		gpio_toggle(GPIOC, GPIO13);
-		vTaskDelay(pdMS_TO_TICKS(500));
+		sleep(1);
 	}
+	return NULL;
 }
 
 int main(void)
 {
+	pthread_t blink_thread;
+
 	rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_3V3_96MHZ]);
 
 	rcc_periph_clock_enable(RCC_GPIOC);
@@ -28,9 +31,7 @@ int main(void)
 		GPIO_PUPD_NONE,
 		GPIO13);
 
-	xTaskCreateStatic(
-		blink, "LED", configMINIMAL_STACK_SIZE, NULL,
-		configMAX_PRIORITIES-1, taskStack, &taskBuf);
+	pthread_create(&blink_thread, NULL, blink, NULL);
 
 	vTaskStartScheduler(); // FreeRTOS, take the wheel!
 	configASSERT(0); // shouldn't get here
