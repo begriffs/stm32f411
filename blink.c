141f2f30403a4121dc5a26d18a939f955c108b08
Joe Nelson <joe@begriffs.com>
2022-11-28 22:46:47 -0600

    Convert blinky for Nucleo

--- blink.c
+++ blink.c
@@ -4,6 +4,7 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 
+/* freertos uses this for configCPU_CLOCK_HZ */
 uint32_t SystemCoreClock;
 
 void app(void *args)
@@ -11,7 +12,7 @@ void app(void *args)
 	(void)args;
 	while (1)
 	{
-		gpio_toggle(GPIOC, GPIO13);
+		gpio_toggle(GPIOA, GPIO5);
 		vTaskDelay(pdMS_TO_TICKS(500));
 	}
 }
@@ -24,12 +25,8 @@ int main(void)
 	rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_3V3_96MHZ]);
 	SystemCoreClock = 96000000;
 
-	rcc_periph_clock_enable(RCC_GPIOC);
-	gpio_mode_setup(
-		GPIOC,
-		GPIO_MODE_OUTPUT,
-		GPIO_PUPD_NONE,
-		GPIO13);
+	rcc_periph_clock_enable(RCC_GPIOA);
+	gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO5);
 
 	xTaskCreateStatic(
 		app, "app", configMINIMAL_STACK_SIZE, NULL,
